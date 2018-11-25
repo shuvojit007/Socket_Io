13dd048b565a9cad21bc03591131fff166ae9dab
java <thejavalover007@gmail.com>
2018-11-25 18:45:09 +0600

    new commit

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ io.on("connection", socket => {
   }
 });
 
-server.listen(3000, () => {
+
+
+var port = process.env.PORT || 3000;
+server.listen(port, () => {
   console.log("Node app is running on port 3000");
 });
